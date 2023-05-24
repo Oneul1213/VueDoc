edea0b1d2c7c17c4b0c7660cb2bc73efb77736b4
Oneul1213 <toda1213@naver.com>
2023-05-24 16:18:17 +0900

    [Vue3] 컴포넌트 심화 - 이벤트까지

--- Vue3/vue3-doc/src/App.vue
+++ Vue3/vue3-doc/src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-    <template-refs></template-refs>
+  <div :style="{ fontSize: postFontSize + 'em' }">
+    <BlogPost
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      @enlarge-text="postFontSize += 0.1"
+    />
   </div>
 </template>
 
 <script setup>
-import TemplateRefs from "./components/TemplateRefs.vue";
+import BlogPost from './components/BlogPost.vue';
+import { ref } from 'vue'
+
+const posts = ref([
+  { id: 1, title: 'Vue와 함께한 나의 여행' },
+  { id: 2, title: 'Vue로 블로깅하기' },
+  { id: 3, title: 'Vue가 재밌는 이유' },
+])
+
+const postFontSize = ref(1)
 </script>
 
 <style lang="scss" scoped>
---
+++ Vue3/vue3-doc/src/components/BlogPost.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="blog-post">
+        <h4>{{ title }}</h4>
+        <button @click="$emit('enlarge-text')">텍스트 확대</button>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps(['title'])
+const emit = defineEmits(['enlarge-text'])
+
+console.log(props.title)
+// emit('enlarge-text')
+</script>
+
+<style lang="scss" scoped>
+
+</style>

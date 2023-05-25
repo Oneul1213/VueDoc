c1f188d3af3d66f8aae85e9ba9a4f96ec9fa0dee
Oneul1213 <toda1213@naver.com>
2023-05-25 11:25:25 +0900

    [Vue3] 슬롯까지

--- Vue3/vue3-doc/src/App.vue
+++ Vue3/vue3-doc/src/App.vue
@@ -1,56 +1,30 @@
 <template>
   <div>
-    <CustomInput v-model="searchText" />
-    <p>{{ searchText }}</p>
-    <br />
+    <FancyButton>
+      클릭하기!
+    </FancyButton>
 
-    <MyComponent v-model:title="bookTitle" />
-    <p>{{ bookTitle }}</p>
-    <br/>
+    <BaseLayout>
+      <template #header>
+        <h1>다음은 페이지 제목일 수 있습니다.</h1>
+      </template>
+      <p>주요 내용에 대한 단락입니다.</p>
+      <p>그리고 또 하나.</p>
+      <template #footer>
+        <p>다음은 연락처 정보입니다.</p>
+      </template>
+    </BaseLayout>
 
-    <UserName
-      v-model:first-name="first"
-      v-model:last-name="last"
-    />
-    <p>{{ fullName }}</p>
-    <br/>
-
-    <MyComponent2 v-model.capitalize="myText"/>
-    <p>{{ myText }}</p>
-
-    <MyButton class="large" @click="click" />
-
-    <CustomLayout id="custom-layout" @click="changeValue" />
+    <MyComponent3 v-slot="{ text, count }">
+      {{ text }} {{ count }}
+    </MyComponent3>
   </div>
 </template>
 
 <script setup>
-import CustomLayout from './components/CustomLayout.vue';
-import MyButton from './components/MyButton.vue';
-import MyComponent2 from './components/MyComponent2.vue';
-import UserName from './components/UserName.vue';
-import MyComponent from './components/MyComponent.vue';
-import CustomInput from './components/CustomInput.vue';
-import { ref, computed } from 'vue'
-
-
-const searchText = ref('')
-const bookTitle = ref('')
-const first = ref('')
-const last = ref('')
-const myText = ref('')
-
-
-const fullName = computed(() => first.value + ' ' + last.value)
-
-
-function click(e) {
-  console.log('hi')
-}
-
-function changeValue(e) {
-  console.log('changeValue')
-}
+import MyComponent3 from './components/MyComponent3.vue';
+import BaseLayout from './components/BaseLayout.vue';
+import FancyButton from './components/FancyButton.vue';
 </script>
 
 <style lang="scss" scoped>
---
+++ Vue3/vue3-doc/src/components/MyComponent3.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <slot :text="greetingMessage" :count="1"></slot>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const greetingMessage = ref('hi')
+</script>
+
+<style lang="scss" scoped>
+
+</style>

f6598d77011176ad440957635b960ba6ce3472e8
Oneul1213 <toda1213@naver.com>
2023-05-25 14:00:18 +0900

    [Vue3] 컴포저블이란?

--- Vue3/vue3-doc/src/App.vue
+++ Vue3/vue3-doc/src/App.vue
@@ -1,35 +1,11 @@
 <template>
-  <div>
-    <FancyButton>
-      클릭하기!
-    </FancyButton>
-
-    <BaseLayout>
-      <template #header>
-        <h1>다음은 페이지 제목일 수 있습니다.</h1>
-      </template>
-      <p>주요 내용에 대한 단락입니다.</p>
-      <p>그리고 또 하나.</p>
-      <template #footer>
-        <p>다음은 연락처 정보입니다.</p>
-      </template>
-    </BaseLayout>
-
-    <MyComponent3 v-slot="{ text, count }">
-      {{ text }} {{ count }}
-    </MyComponent3>
-  </div>
+  마우스 위치: {{ x }}, {{ y }}
 </template>
 
 <script setup>
-import MyComponent3 from './components/MyComponent3.vue';
-import BaseLayout from './components/BaseLayout.vue';
-import FancyButton from './components/FancyButton.vue';
-import { defineAsyncComponent } from 'vue';
+import { useMouse } from '@/mouse.js'
 
-const AsyncComp = defineAsyncComponent(() => {
-  import('./components/MyComponent.vue')
-})
+const { x, y } = useMouse()
 </script>
 
 <style lang="scss" scoped>

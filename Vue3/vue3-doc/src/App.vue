8faeeb5068bfee322cc282f94339c42e6681f8c7
Oneul1213 <toda1213@naver.com>
2023-05-25 15:13:48 +0900

    [Vue3] 재사용성 - 커스텀 디렉티브까지

--- Vue3/vue3-doc/src/App.vue
+++ Vue3/vue3-doc/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  마우스 위치: {{ x }}, {{ y }}
+  <div>
+    <CustomDirective />
+  </div>
 </template>
 
 <script setup>
-import { useMouse } from '@/mouse.js'
-
-const { x, y } = useMouse()
+import CustomDirective from './components/CustomDirective.vue'
 </script>
 
 <style lang="scss" scoped>
---
+++ Vue3/vue3-doc/src/components/CustomDirective.vue
@@ -0,0 +1,13 @@
+<template>
+    <input v-focus />
+</template>
+
+<script setup>
+const vFocus = {
+    mounted: (el) => el.focus()
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
